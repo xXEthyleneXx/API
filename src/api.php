ac7f97e58bf78fc6eb049898d788bd4ae6a9d4be
Devon Edwards <xXEthyleneXx@gmail.com>
2023-05-13 09:30:14 -0400

    Update api.php

--- src/api.php
+++ src/api.php
@@ -3,24 +3,31 @@ declare(strict_types=1);
 namespace xXEthyleneXx;
 require "load.php";
 
+use xXEthyleneXx\Config\Config;
 use xXEthyleneXx\Config\MariaDBC;
+use xXEthyleneXx\Config\RedisDBC;
 use xXEthyleneXx\Database\MariaDBD;
 use xXEthyleneXx\Database\RedisD;
 use xXEthyleneXx\Exceptions\API_Exception;
 use xXEthyleneXx\Logging;
 
 class API {
+    // Generators
     use Gens;
-    // Public
-    public MariaDBC $MariaDB;
-
+    // Configs
+    protected Config $config;
+    use MariaDBC;
+    public MariaDBC $MariaDBC;
+    use RedisDBC;
+    public RedisDBC $RedisDBC;
     /**
      * Construct API
      * 
      * @param string $file_path = "/path/to/config.json"
      */
     public function __construct(string $file_path) {
-        $this->MariaDB = new MariaDBC($file_path);
+        $this->config = new Config($file_path);
+        echo($this->MariaDBC->ALL);
     }
 }
 ?>
